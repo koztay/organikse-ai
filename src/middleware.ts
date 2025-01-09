d1cbcae31fb43bdac63eacc96cfc1b0cdb72d42a
Kemal ÖZTAY <koztay@me.com>
2025-01-09 18:24:46 +0300

    we are still fighting with the errors

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,6 @@ import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export const runtime = 'experimental-edge'
-
 export async function middleware(request: NextRequest) {
   const res = NextResponse.next()
   const supabase = createMiddlewareClient({ req: request, res })
@@ -21,13 +19,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
     '/((?!api|_next/static|_next/image|favicon.ico).*)',
   ],
 } 
